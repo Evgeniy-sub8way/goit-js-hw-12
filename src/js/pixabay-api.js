963a54514e51c7b6c134d19ee919d336d9887a6a
Evgeniy-Sub8way <sub8way@gmail.com>
2026-03-22 16:37:00 +0200

    3try

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '55036572-7370ebd0316836cff88f01645';
 
-export async function getImagesByQuery(query) {
+export async function getImagesByQuery(query, page) {
   const response = await axios.get('/', {
     params: {
       key: API_KEY,
@@ -11,6 +11,8 @@ export async function getImagesByQuery(query) {
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
+      page, // 🔹 номер сторінки
+      per_page: 15, // 🔹 кількість результатів
     },
   });
   return response.data;
